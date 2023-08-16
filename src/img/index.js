449d1c3837d6071ed6671320d394eb22f60cb0f2
jielongjp <jdan000@gmail.com>
2023-08-16 22:23:26 +0900

    add cats, refactor wildlife page

---
+++ src/img/index.js
@@ -0,0 +1,42 @@
+// wildlife folder
+import Cat1 from './wildlife/cat-1.jpg';
+import Cat2 from './wildlife/cat-2.jpg';
+import Cat3 from './wildlife/cat-3.jpg';
+import Cat4 from './wildlife/cat-4.jpg';
+import Cat5 from './wildlife/cat-5.jpg';
+import Cat6 from './wildlife/cat-6.jpg';
+import Cat7 from './wildlife/cat-7.jpg';
+import Cat8 from './wildlife/cat-8.jpg';
+import Cat9 from './wildlife/cat-9.jpg';
+import Cat10 from './wildlife/cat-10.jpg';
+import Cat11 from './wildlife/cat-11.jpg';
+import Cat13 from './wildlife/cat-13.jpg';
+import Cat14 from './wildlife/cat-14.jpg';
+import Cat15 from './wildlife/cat-15.jpg';
+import Cat16 from './wildlife/cat-16.jpg';
+import Cat17 from './wildlife/cat-17.jpg';
+import Cat18 from './wildlife/cat-18.jpg';
+import Cat19 from './wildlife/cat-19.jpg';
+import Cat20 from './wildlife/cat-20.jpg';
+import CatGroup from './wildlife/catgroup.jpg';
+
+// portfolio folder
+import Image1 from './portfolio/1.png';
+import Image2 from './portfolio/2.png';
+import Image3 from './portfolio/3.png';
+import Image4 from './portfolio/4.png';
+import Image5 from './portfolio/5.png';
+import Image6 from './portfolio/6.png';
+import Image7 from './portfolio/7.png';
+import Image8 from './portfolio/8.png';
+import Image9 from './portfolio/9.png';
+import Image10 from './portfolio/10.png';
+import Image11 from './portfolio/11.png';
+import Image12 from './portfolio/12.png';
+import Image13 from './portfolio/13.png';
+import Image14 from './portfolio/14.png';
+import Image15 from './portfolio/15.png';
+import Image16 from './portfolio/16.png';
+
+export const CatImages = [Cat1, Cat2, Cat3, Cat4, Cat5, Cat6, Cat7, Cat8, Cat9, Cat10, Cat11, Cat13, Cat14, Cat15, Cat16, Cat17, Cat18, Cat19, Cat20, CatGroup];
+export const PortfolioImages = [Image1, Image2, Image3, Image4, Image5, Image6, Image7, Image8, Image9, Image10, Image11, Image12, Image13, Image14, Image15, Image16];
--- src/pages/Wildlife.js
+++ src/pages/Wildlife.js
@@ -1,16 +1,5 @@
 import React from 'react';
-import Image1 from '../img/portfolio/1.png';
-import Image2 from '../img/portfolio/2.png';
-import Image3 from '../img/portfolio/3.png';
-import Image4 from '../img/portfolio/4.png';
-import Cat1 from '../img/wildlife/cat-1.jpg'
-import Cat2 from '../img/wildlife/cat-2.jpg'
-import Cat3 from '../img/wildlife/cat-3.jpg'
-import Cat4 from '../img/wildlife/cat-4.jpg'
-import Cat5 from '../img/wildlife/cat-5.jpg'
-import Cat6 from '../img/wildlife/cat-6.jpg'
-import Cat7 from '../img/wildlife/cat-7.jpg'
-import Catgroup from '../img/wildlife/catgroup.jpg'
+import { CatImages } from '../img';
 import ImageGrid from '../components/ImageGrid';
 import { Link } from 'react-router-dom';
 import { motion } from 'framer-motion';
@@ -51,13 +40,13 @@ const Wildlife = () => {
           </motion.div>
           {/* image grid */}
           <div className='flex-1 max-h-96 lg:max-h-max order-2 lg:order-none'>
-            <img src={Cat1} alt='' />
+            <img src={CatImages[0]} alt='' />
           </div>
         </div>
       </div>
-      < ImageGrid image1={Cat4} image2={Cat2} image3={Cat3} image4={Catgroup} image5={Cat5} image6={Cat6} image7={Cat7}/>
-      < ImageGrid image1={Cat4} image2={Cat2} image3={Cat3} image4={Catgroup} image5={Cat5} image6={Cat6} image7={Cat7}/>
-
+        <ImageGrid image1={CatImages[3]} image2={CatImages[1]} image3={CatImages[2]} image4={CatImages[4]} image5={CatImages[16]} image6={CatImages[5]} image7={CatImages[6]}/>
+        <ImageGrid image1={CatImages[9]} image2={CatImages[18]} image3={CatImages[8]} image4={CatImages[7]} image5={CatImages[10]} image6={CatImages[0]} image7={CatImages[12]}/>
+        <ImageGrid image1={CatImages[13]} image2={CatImages[14]} image3={CatImages[15]} image4={CatImages[17]} image5={CatImages[18]} image6={CatImages[19]} image7={CatImages[16]}/>
     </motion.section>
   );
 };
