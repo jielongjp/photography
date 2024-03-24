b90a367ae66c5094e6a975d94844dc9cd75db89b
“jielongjp” <82949450+jielongjp@users.noreply.github.com>
2024-03-24 20:32:26 +0000

    adjust

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -7,7 +7,7 @@ const Footer = () => {
     <>
       <footer className="px-4 lg:px-10 py-6 lg:py-8 flex flex-col lg:flex-row items-center justify-between bg-[#004d53] text-[#f1b472] mt-10">
         <div className="flex md:flex-row items-top w-full lg:w-auto mb-4 lg:mb-0 flex-wrap justify-center">
-          <div className="grid grid-cols-2 sm:pr-10 text-center sm:text-left">
+          <div className="grid grid-cols-2 sm:pr-10 text-center lg:text-left">
             <a
               href="https://jordharris.com"
               className="text-xl hover:text-white mr-4 lg:mr-8 mb-2 lg:mb-0"
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -46,14 +46,15 @@ const Home = () => {
               className="pt-8 pb-8 lg:pl-14 pl-4 lg:pt-0 lg:pb-0 lg:w-auto z-10 lg:absolute flex flex-col justify-center items-start"
             >
               <h1 className="h1">
-                photographer <br /> & web developer
+                photographer <br /> & web
+                {window.innerWidth < 512 ? <br /> : ""} developer
               </h1>
               <p className="text-[26px] lg:text-[36px] font-primary mb-4 lg:mb-12">
                 Jordan Leigh Harris
               </p>
               <a
                 href="https://jordharris.com/contact/"
-                className="btn rounded mb-[30px] hover:bg-slate-700"
+                className="btn rounded mb-[30px] bg-[#004d53] hover:bg-[#00808b]"
               >
                 contact me
               </a>
