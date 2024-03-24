5d20d0c592b648dd24e54e634187128c22115267
“jielongjp” <82949450+jielongjp@users.noreply.github.com>
2024-03-24 20:24:36 +0000

    edit footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,44 +5,27 @@ import Socials from "./Socials";
 const Footer = () => {
   return (
     <>
-      <footer className="px-4 lg:px-10 py-6 lg:py-8 flex flex-col lg:flex-row items-center justify-between bg-[#05203c] text-gray-200 mt-10">
+      <footer className="px-4 lg:px-10 py-6 lg:py-8 flex flex-col lg:flex-row items-center justify-between bg-[#004d53] text-[#f1b472] mt-10">
         <div className="flex md:flex-row items-top w-full lg:w-auto mb-4 lg:mb-0 flex-wrap justify-center">
-          <div className="flex sm:pr-10">
+          <div className="grid grid-cols-2 sm:pr-10 text-center sm:text-left">
             <a
               href="https://jordharris.com"
               className="text-xl hover:text-white mr-4 lg:mr-8 mb-2 lg:mb-0"
             >
               Main site
-              <p className="text-xs ">jordharris.com</p>
+              <p className="text-xs">jordharris.com</p>
             </a>
             <a
               href="https://learntaigi.com"
               className="inline-block text-xl hover:text-white mr-4 lg:mr-8 mb-2 lg:mb-0"
             >
-              learntaigi.com
-              <p className="text-xs ">Taiwanese language learning</p>
+              Learn Taiwanese
+              <p className="text-xs ">with learntaigi.com</p>
             </a>
+            <p className="col-span-2 text-xs border-t border-[#f1b472] mt-4 pt-2">
+              © All images subject to copyright
+            </p>
           </div>
-          {/* <nav className=" gap-x-12 font-semibold sm:flex hidden">
-            <Link to={"/"} className="text-white hover:text-gray-400">
-              Home
-            </Link>
-            <Link to={"/travel/"} className="text-white hover:text-gray-400">
-              Travel
-            </Link>
-            <Link to={"/wildlife/"} className="text-white hover:text-gray-400">
-              Wildlife
-            </Link>
-            <Link to={"/about/"} className="text-white hover:text-gray-400">
-              About
-            </Link>
-            <a
-              href="https://jordharris.com/contact/"
-              className="text-white hover:text-gray-400"
-            >
-              Contact
-            </a>
-          </nav> */}
         </div>
         <Socials />
       </footer>
