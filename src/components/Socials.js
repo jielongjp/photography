b3bbbb08a739d49d671766944354452469c52805
“jielongjp” <82949450+jielongjp@users.noreply.github.com>
2024-03-26 19:38:23 +0000

    change style

--- src/components/Socials.js
+++ src/components/Socials.js
@@ -6,22 +6,22 @@ const Socials = () => {
   return (
     <div className="flex lg:ml-24">
       <ul className="flex gap-x-4 text-white">
-        <li>
+        <li className="hover:text-[#f1b472]">
           <a href="https://www.linkedin.com/in/jordanharris00/" target="_blank">
             <ImLinkedin />
           </a>
         </li>
-        <li>
+        <li className="hover:text-[#f1b472]">
           <a href="https://github.com/jielongjp" target="_blank">
             <ImGithub />
           </a>
         </li>
-        <li>
+        <li className="hover:text-[#f1b472]">
           <a href="https://www.instagram.com/jielong_/" target="_blank">
             <ImInstagram />
           </a>
         </li>
-        <li>
+        <li className="hover:text-[#f1b472]">
           <a
             href="https://www.youtube.com/channel/UC1qHXN-WjKK1uZ6yfemUdhg/"
             target="_blank"
