1a135c264825c03ad6b684d27eea9fd4e9ebdb2a
“jielongjp” <82949450+jielongjp@users.noreply.github.com>
2024-05-29 19:22:26 +0100

    change text

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,46 +1,44 @@
-import React from 'react';
-import Headshot from '../img/home/jordan-headshot.png';
-import { motion } from 'framer-motion';
-import { transition1 } from '../transitions';
+import React from "react";
+import Headshot from "../img/home/jordan-headshot.png";
+import { motion } from "framer-motion";
+import { transition1 } from "../transitions";
 
 const About = () => {
   return (
     <motion.section
-      initial={{ opacity: 0, y: '100%' }}
+      initial={{ opacity: 0, y: "100%" }}
       animate={{ opacity: 1, y: 0 }}
-      exit={{ opacity: 0, y: '100%' }}
+      exit={{ opacity: 0, y: "100%" }}
       transition={transition1}
-      className='section'
+      className="section"
     >
-      <div
-        className='container mx-auto h-full relative'
-      >
+      <div className="container mx-auto h-full relative">
         {/* text & img wrapper */}
-        <div className='flex flex-col lg:flex-row h-full items-center justify-center gap-x-24 text-center lg:text-left lg:pt-16'>
+        <div className="flex flex-col lg:flex-row h-full items-center justify-center gap-x-24 text-center lg:text-left lg:pt-16">
           {/* image */}
-          <div className='flex-1 max-h-96 lg:max-h-max order-2 lg:order-none'>
-            <img src={Headshot} alt='' />
+          <div className="flex-1 max-h-96 lg:max-h-max order-2 lg:order-none">
+            <img src={Headshot} alt="" />
           </div>
           {/* text */}
           <motion.div
-            initial={{ opacity: 0, y: '-80%' }}
+            initial={{ opacity: 0, y: "-80%" }}
             animate={{ opacity: 1, y: 0 }}
-            exit={{ opacity: 0, y: '-80%' }}
+            exit={{ opacity: 0, y: "-80%" }}
             transition={transition1}
-            className='flex-1 pt-36 pb-14 lg:pt-0 lg:w-auto z-10 flex flex-col justify-center items-center lg:items-start pl-1'
+            className="flex-1 pt-36 pb-14 lg:pt-0 lg:w-auto z-10 flex flex-col justify-center items-center lg:items-start pl-1"
           >
-            <h1 className='h1'>About me</h1>
-            <p className='mb-12 max-w-sm'>
-              I am a web developer living in Tokyo, Japan.
-              <b> Photography</b> has always been a passion of mine.
-              along with part of my work in the past.
+            <h1 className="h1">About me</h1>
+            <p className="mb-12 max-w-sm">
+              I am a web developer living in the UK.
+              <b> Photography</b> has always been a passion of mine. along with
+              part of my work in the past.
               <br />
               <br />
-              Lorem ipsum, dolor sit amet consectetur adipisicing elit.
-              Recusandae, iure! Ullam, dolore? Eligendi, quam mollitia.
+              While I mostly enjoy travel photography and documenting my
+              journeys, I have experience with a range of different photography.
             </p>
-            <a href='https://jordharris.com' className='btn'>
-              View my work
+            <a href="https://jordharris.com" className="btn">
+              View my development work
             </a>
           </motion.div>
         </div>
